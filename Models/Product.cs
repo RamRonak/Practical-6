7fc8a432e6efa5456cc447ae843562c41288ef0b
RamRonak <ronak.ram133234@marwadiuniversity.ac.in>
2026-09-08 11:40:33 +0530

    Add files via upload

---
+++ Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        public string Description { get; set; }
+    }
+}
